9af1745df57cc5d2a269edfe0ee8b37d7ffb2378
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-11-12 13:05:01 +0500

    authForms

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,10 +1,105 @@
-import React from 'react'
+"use client"
+
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
+import Image from "next/image"
+
+
+const formSchema = z.object({
+    fullName: z.string().min(2).max(50),
+    email: z.string().min(2).max(50),
+})
+
 
 type FormType = "sign-in" | "sign-up"
 
 const AuthForm = ({ type }: { type: FormType }) => {
+    const [isLoading, setIsLoading] = useState(false)
+    const [errorMessage, setErrorMessage] = useState("")
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            fullName: "",
+            email: "",
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
     return (
-        <div>AuthForm</div>
+        <>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+                    <h1 className="form-title">
+                        {type === 'sign-in' ? "Sign In" : "Sign Up"}
+                    </h1>
+                    {
+                        type === 'sign-up' &&
+                        <FormField
+                            control={form.control}
+                            name="fullName"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <div className="shad-form-item">
+                                        <FormLabel className="shad-form-label">Full Name</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Enter Your Full Name" {...field} className="shad-input" />
+                                        </FormControl>
+                                    </div>
+                                    <FormMessage className="shad-form-message" />
+                                </FormItem>
+                            )}
+                        />
+                    }
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <div className="shad-form-item">
+                                    <FormLabel className="shad-form-label">Email</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Enter Your Email" {...field} className="shad-input" />
+                                    </FormControl>
+                                </div>
+                                <FormMessage className="shad-form-message" />
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit" className="form-submit-button" disabled={isLoading}>
+                        {type === 'sign-in' ? "Sign In" : "Sign Up"}
+                        {
+                            isLoading && (<Image src={`/assets/icons/loader.svg`} alt="loader" width={24} height={24} className="animate-spint ml-2" />)
+                        }
+                    </Button>
+                    {
+                        errorMessage &&
+                        (
+                            <p className="error-message">*{errorMessage}</p>
+                        )
+                    }
+                </form>
+            </Form>
+        </>
     )
 }
 
