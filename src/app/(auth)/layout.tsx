9335f5bd77ea84bb6c4014bc6a1e1c865033aaf0
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-11-12 11:59:15 +0500

    layout auth

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,29 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className='flex min-h-screen'>
-            {children}
+            <section className='bg-brand p-10 hidden w-1/2 lg:flex xl:w-2/5 items-center justify-center'>
+                <div className='flex flex-col items-center justify-center space-y-12 max-h-[800px] max-w-[430px]'>
+                    <div className='flex items-center gap-4'>
+                        <Image src={`/favicon.ico`} alt='logo' width={80} height={80} className='h-auto' />
+                        <h1 className='h1 text-white'>ArcStorage</h1>
+                    </div>
+                    <div className='space-y-5 text-white'>
+                        <h1 className='h1'>
+                            Manage your files the best way
+                        </h1>
+                        <p className='body-1'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum molestiae libero perspiciatis quaerat harum inventore fuga officiis.
+                        </p>
+                    </div>
+                    <Image src={`/assets/images/files.png`} alt='files' width={300} height={300} className='transition-all hover:rotate-2 hover:scale-105' />
+                </div>
+            </section>
+            <section className=''>
+                {children}
+            </section>
         </div>
     )
 }
