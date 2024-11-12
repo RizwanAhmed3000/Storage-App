141bdc417ac8ecfd3bb29838459253b7b0487207
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-11-12 12:07:39 +0500

    auth layout done

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,8 +6,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <div className='flex min-h-screen'>
             <section className='bg-brand p-10 hidden w-1/2 lg:flex xl:w-2/5 items-center justify-center'>
                 <div className='flex flex-col items-center justify-center space-y-12 max-h-[800px] max-w-[430px]'>
-                    <div className='flex items-center gap-4'>
-                        <Image src={`/favicon.ico`} alt='logo' width={80} height={80} className='h-auto' />
+                    <div className='flex w-[100%]'>
+                        {/* <Image src={`/assets/icons/logo-brand.svg`} alt='logo' width={80} height={80} className='h-auto' /> */}
                         <h1 className='h1 text-white'>ArcStorage</h1>
                     </div>
                     <div className='space-y-5 text-white'>
@@ -21,7 +21,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
                     <Image src={`/assets/images/files.png`} alt='files' width={300} height={300} className='transition-all hover:rotate-2 hover:scale-105' />
                 </div>
             </section>
-            <section className=''>
+            <section className='flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0'>
                 {children}
             </section>
         </div>
