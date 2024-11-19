03e3cd0aa2f0c047a01be2b7307972a3537e7f2a
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-11-19 12:35:33 +0500

    file uploader UI

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -1,8 +1,57 @@
-import React from 'react'
+'use client'
+import React, { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { Button } from './ui/button';
+import { cn, getFileType } from '@/lib/utils';
+import Image from 'next/image';
 
-const FileUploader = () => {
+interface Props {
+    className?: string;
+    ownerId: string;
+    accountId: string
+}
+
+const FileUploader = ({ className, ownerId, accountId }: Props) => {
+    const [files, setFiles] = useState<File[]>([])
+
+    const onDrop = useCallback(async (acceptedFiles: File[]) => {
+        // Do something with the files
+        setFiles(acceptedFiles)
+    }, [])
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
     return (
-        <div>FileUploader</div>
+        <div {...getRootProps()} className='cursor-pointer'>
+            <input {...getInputProps()} />
+            <Button className={cn('uploader-button', className)}>
+                <Image src={`/assets/icons/upload.svg`} alt='upload' width={24} height={24} />
+                <p>Upload</p>
+            </Button>
+            {
+                files.length > 0 && (
+                    <ul className='uploader-preview-list'>
+                        <h4 className='h4 text-light-100'>Uploading</h4>
+                        {
+                            files.map((file, index) => {
+                                const { type, extension } = getFileType(file.name)
+
+                                return (
+                                    <li className='uploader-preview-item' key={`${file.name}-${index}`}>
+                                        test
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                )
+            }
+            {
+                isDragActive ?
+                    <p>Drop the files here ...</p> :
+                    <p>Drag 'n' drop some files here, or click to select files</p>
+            }
+        </div>
     )
 }
 
