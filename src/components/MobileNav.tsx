237e96730270430020e73a603d20829cf2e660a4
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-11-19 12:01:13 +0500

    signout handler

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -16,6 +16,7 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
 import FileUploader from "./FileUploader";
+import { signOutUser } from "@/lib/actions/user.actions";
 
 interface Props {
     fullName: string;
@@ -72,7 +73,7 @@ const MobileNav = ({ fullName, email, avatar, ownerId, accountId }: Props) => {
 
                     <div className="flex flex-col justify-between gap-5 pb-5">
                         <FileUploader />
-                        <Button type='submit' className='mobile-sign-out-button'>
+                        <Button type='submit' className='mobile-sign-out-button' onClick={async () => await signOutUser()}>
                             <Image src={`/assets/icons/logout.svg`} alt='logo' width={24} height={24} />
                             <p>Logout</p>
                         </Button>
