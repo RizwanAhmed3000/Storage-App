dd1ee832a4fac921bfe03c36286ee946ee79dd0b
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-11-20 11:43:59 +0500

    file thumbnail

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -1,14 +1,23 @@
+import { cn, getFileIcon } from '@/lib/utils';
+import Image from 'next/image';
 import React from 'react'
 
 interface Props {
     type: string;
     extension: string;
-    url?: string
+    url?: string;
+    imageClassName?: string;
+    className?: string;
 }
 
-const Thumbnail = ({ type, extension, url = '' }: Props) => {
+const Thumbnail = ({ type, extension, url = '', imageClassName, className }: Props) => {
+
+    const isImage = type === 'image' && extension !== 'svg';
+
     return (
-        <div>Thumbnail</div>
+        <figure className={cn('thumbnail', className)}>
+            <Image src={isImage ? url : getFileIcon(extension, type)} alt='thumbnail' width={100} height={100} className={cn('size-8 object-contain', imageClassName, isImage && 'thumbnail-image')} />
+        </figure>
     )
 }
 
