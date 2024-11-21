33d80bd89edeb8cb883d4187d961457af3f70cb3
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-11-21 13:02:01 +0500

    constants and types

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -27,3 +27,5 @@ export const navItems = [
 ]
 
 export const avatarPlaceholder = 'https://media.istockphoto.com/id/2151669184/vector/vector-flat-illustration-in-grayscale-avatar-user-profile-person-icon-gender-neutral.jpg?s=612x612&w=0&k=20&c=UEa7oHoOL30ynvmJzSCIPrwwopJdfqzBs0q69ezQoM8='
+
+export const MAX_FILE_SIZE = 50 * 1024 * 1024; // 50MB
