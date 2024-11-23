481a9f3c52c4cbc7672604944733607e581474f3
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-11-23 12:27:09 +0500

    fetch files

--- src/app/(root)/[type]/page.tsx
+++ src/app/(root)/[type]/page.tsx
@@ -1,9 +1,14 @@
 import Sort from '@/components/Sort';
+import { getFiles } from '@/lib/actions/file.actions';
+import { Models } from 'node-appwrite';
 import React from 'react'
 
 const page = async ({ params }: SearchParamProps) => {
     const type = (await params)?.type as string || "";
     // console.log("type ==>> ", type)
+
+    const files = await getFiles();
+
     return (
         <div className='page-container'>
             <section className="w-full">
@@ -23,7 +28,19 @@ const page = async ({ params }: SearchParamProps) => {
 
             {/* Render the files */}
 
-            
+            {files.total > 0 ? (
+                <section className='file-list'>
+                    {
+                        files.documents.map((file: Models.Document)=>(
+                            <h1 key={file.$id} className='h1'>
+                                {file.name}
+                            </h1>
+                        ))
+                    }
+                </section>
+            ) : (
+                <p className="empty-list">No files uploaded</p>
+            )}
         </div>
     )
 }
