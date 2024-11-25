9aff2078501687fd4935c777510581ac3a6fccfb
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-11-25 12:35:08 +0500

    action dropdown const

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -26,6 +26,61 @@ export const navItems = [
     },
 ]
 
+export const actionsDropdownItems = [
+    {
+        label: 'Rename',
+        icon: '/assets/icons/edit.svg',
+        value: 'rename',
+    },
+    {
+        label: 'Details',
+        icon: '/assets/icons/info.svg',
+        value: 'details',
+    },
+    {
+        label: 'Share',
+        icon: '/assets/icons/share.svg',
+        value: 'share',
+    },
+    {
+        label: 'Download',
+        icon: '/assets/icons/download.svg',
+        value: 'download',
+    },
+    {
+        label: 'Delete',
+        icon: '/assets/icons/delete.svg',
+        value: 'delete',
+    },
+];
+
+export const sortTypes = [
+    {
+        label: 'Date created (newest)',
+        value: '$createdAt-desc',
+    },
+    {
+        label: 'Created Date (oldest)',
+        value: '$createdAt-asc',
+    },
+    {
+        label: 'Name (A-Z)',
+        value: 'name-asc',
+    },
+    {
+        label: 'Name (Z-A)',
+        value: 'name-desc',
+    },
+    {
+        label: 'Size (Highest)',
+        value: 'size-desc',
+    },
+    {
+        label: 'Size (Lowest)',
+        value: 'size-asc',
+    },
+];
+
 export const avatarPlaceholder = 'https://media.istockphoto.com/id/2151669184/vector/vector-flat-illustration-in-grayscale-avatar-user-profile-person-icon-gender-neutral.jpg?s=612x612&w=0&k=20&c=UEa7oHoOL30ynvmJzSCIPrwwopJdfqzBs0q69ezQoM8='
 
 export const MAX_FILE_SIZE = 50 * 1024 * 1024; // 50MB
