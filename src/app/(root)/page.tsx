fb505a774bc642fad09e471f61c0701da7e515b1
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-12-03 12:52:05 +0500

    dashboard summry & recent items

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,104 @@
 // import Image from "next/image";
 
-export default function Home() {
+import ActionDropdown from "@/components/ActionDropdown";
+import Chart from "@/components/Chart";
+import FormattedDateTime from "@/components/FormattedDateTime";
+import Thumbnail from "@/components/Thumbnail";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { getFiles, totalSpaceUsed } from "@/lib/actions/file.actions";
+import { convertFileSize, getUsageSummary } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { Models } from "node-appwrite";
+
+export default async function Dashboard() {
+
+  const [files, totalSpace] = await Promise.all([
+    getFiles({ types: [], limit: 10 }),
+    totalSpaceUsed(),
+  ]);
+
+  // console.log(files)
+  // console.log(totalSpace)
+
+  const usageSummary = getUsageSummary(totalSpace);
+  // console.log(usageSummary)
+
   return (
-    <div className="flex-center h-screen">
-      <h1 className="text-brand">
-        hello world
-      </h1>
+    <div className="dashboard-container">
+      <section>
+        <Chart />
+
+        <ul className="dashboard-summary-list">
+          {usageSummary.map((summary) => (
+            <Link
+              href={summary.url}
+              key={summary.title}
+              className="dashboard-summary-card"
+            >
+              <div className="space-y-4">
+                <div className="flex justify-between gap-3">
+                  <Image
+                    src={summary.icon}
+                    width={100}
+                    height={100}
+                    alt="uploaded image"
+                    className="summary-type-icon"
+                  />
+                  <h4 className="summary-type-size">
+                    {convertFileSize(summary.size) || 0}
+                  </h4>
+                </div>
+
+                <h5 className="summary-type-title">{summary.title}</h5>
+                <Separator className="bg-light-400" />
+                <p className="subtitle-1 text-light-200 text-center">Last Updated</p>
+                <FormattedDateTime
+                  date={summary.latestDate}
+                  className="text-center"
+                />
+              </div>
+            </Link>
+          ))}
+        </ul>
+      </section>
+
+      <section className="dashboard-recent-files">
+        <h2 className="h3 xl:h2 text-light-100">Recent files uploaded</h2>
+        {files.documents.length > 0 ? (
+          <ul className="mt-5 flex flex-col gap-5">
+            {files.documents.map((file: Models.Document) => (
+              <Link
+                href={file.url}
+                target="_blank"
+                className="flex items-center gap-3"
+                key={file.$id}
+              >
+                <Thumbnail
+                  type={file.type}
+                  extension={file.extension}
+                  url={file.url}
+                />
+
+                <div className="recent-file-details">
+                  <div className="flex flex-col gap-1">
+                    <p className="recent-file-name">{file.name}</p>
+                    <FormattedDateTime
+                      date={file.$createdAt}
+                      className="caption"
+                    />
+                  </div>
+                  <ActionDropdown file={file} />
+                </div>
+              </Link>
+            ))}
+          </ul>
+        ) : (
+          <p className="empty-list">No files uploaded</p>
+        )}
+      </section>
+
     </div>
   );
 }
